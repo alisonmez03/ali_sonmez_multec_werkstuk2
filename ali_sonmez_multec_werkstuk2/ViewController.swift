581857d733b7ca23c41e2f00dba6633a44b8e4d8
ali.sonmez <ali.sonmmez@student.ehb.be>
2018-05-11 16:49:39 +0200

    Initial Commit

---
+++ ali_sonmez_multec_werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ali_sonmez_multec_werkstuk2
+//
+//  Created by Ali Sönmez on 11/05/2018.
+//  Copyright © 2018 Ali Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
