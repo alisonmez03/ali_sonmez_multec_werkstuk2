b32abc0e133c4c0c7cf41b75e48bd2771c4e99d4
ali.sonmez <ali.sonmmez@student.ehb.be>
2018-05-12 18:18:47 +0200

    Als je op het adres klikt komt er detail tevoorschijn, hardcoded adres om het detail te kunnen testen.

---
+++ ali_sonmez_multec_werkstuk2/Artwork.swift
@@ -0,0 +1,44 @@
+//
+//  Artwork.swift
+//  ali_sonmez_multec_werkstuk2
+//
+//  Created by Ali Sönmez on 12/05/2018.
+//  Copyright © 2018 Ali Sönmez. All rights reserved.
+//
+
+import MapKit
+import Contacts
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+    
+    init?(json: [Any]) {
+        // 1
+        self.title = json[16] as? String ?? "No Title"
+        self.locationName = json[12] as! String
+        self.discipline = json[15] as! String
+        // 2
+        if let latitude = Double(json[18] as! String),
+            let longitude = Double(json[19] as! String) {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = CLLocationCoordinate2D()
+        }
+    }
+}
--- ali_sonmez_multec_werkstuk2/ViewController.swift
+++ ali_sonmez_multec_werkstuk2/ViewController.swift
@@ -7,20 +7,87 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var artworks: [Artwork] = []
 
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
+        
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        
+            centerMapOnLocation(location: initialLocation)
+            
+            let artwork = Artwork(title: "King David Kalakaua",
+                                  locationName: "Waikiki Gateway Park",
+                                  discipline: "Sculpture",
+                                  coordinate: CLLocationCoordinate2D(latitude: 21.282778, longitude: -157.829444))
+            mapView.addAnnotation(artwork)
+        
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+        }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: MKMapViewDelegate {
+    // 1
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 2
+        guard let annotation = annotation as? Artwork else { return nil }
+        // 3
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        // 4
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            // 5
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+    
+    func loadInitialData() {
+        // 1
+        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+            else { return }
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard
+            let data = optionalData,
+            // 2
+            let json = try? JSONSerialization.jsonObject(with: data),
+            // 3
+            let dictionary = json as? [String: Any],
+            // 4
+            let works = dictionary["data"] as? [[Any]]
+            else { return }
+        // 5
+        let validWorks = works.compactMap { Artwork(json: $0) }
+        artworks.append(contentsOf: validWorks)
+    }
 }
 
